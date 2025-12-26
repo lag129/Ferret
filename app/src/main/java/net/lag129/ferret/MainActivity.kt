3048b3d3607ead96c3bde90fb9227cf44cdecccd
lag129 <65485662+lag129@users.noreply.github.com>
2025-12-27 03:55:09 +0900

    ViewModelを追加

--- app/src/main/java/net/lag129/ferret/MainActivity.kt
+++ app/src/main/java/net/lag129/ferret/MainActivity.kt
@@ -5,26 +5,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.auth.Auth
-import io.ktor.client.plugins.auth.providers.BearerTokens
-import io.ktor.client.plugins.auth.providers.bearer
-import io.ktor.client.plugins.cache.HttpCache
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.defaultRequest
-import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.json.Json
 import net.lag129.ferret.ui.theme.FerretTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,9 +26,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             FerretTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val viewModel: ViewModel by viewModels()
+
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        viewModel = viewModel,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
                     )
                 }
             }
@@ -48,51 +42,11 @@ class MainActivity : ComponentActivity() {
 
 @SuppressLint("LocalContextResourcesRead")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-    val context = LocalContext.current
-    val resources = context.resources
-
-    val client = HttpClient(CIO) {
-        defaultRequest {
-            url(resources.getString(R.string.base_url))
-        }
-
-        install(Auth) {
-            bearer {
-                loadTokens {
-                    BearerTokens(
-                        resources.getString(R.string.access_token),
-                        resources.getString(R.string.access_token)
-                    )
-                }
-            }
-        }
-
-        install(ContentNegotiation) {
-            json(Json { ignoreUnknownKeys = true })
-        }
-
-        install(HttpCache)
-    }
-
-    val repository: MastodonRepository = MastodonRepositoryImpl(client)
-
-    LaunchedEffect(Unit) {
-        repository.getHomeTimeline(resources.getString(R.string.id))
-            .onSuccess { statuses ->
-                Napier.d("Fetched ${statuses.size} statuses")
-            }
-            .onFailure { exception ->
-                Napier.e("Error fetching timeline", exception)
-            }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FerretTheme {
-        Greeting("Android")
-    }
+fun Greeting(
+    viewModel: ViewModel,
+    modifier: Modifier = Modifier
+) {
+    val statuses by viewModel.uiState.collectAsState()
+    
+    Napier.d("Fetched ${statuses.size} statuses")
 }
---
+++ app/src/main/java/net/lag129/ferret/ViewModel.kt
@@ -0,0 +1,67 @@
+package net.lag129.ferret
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import io.github.aakira.napier.Napier
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
+import io.ktor.client.plugins.cache.HttpCache
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
+import net.lag129.ferret.api.entity.Status
+
+class ViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val resources = application.resources
+
+    private val client = HttpClient(CIO) {
+        defaultRequest {
+            url(resources.getString(R.string.base_url))
+        }
+        install(Auth) {
+            bearer {
+                loadTokens {
+                    BearerTokens(
+                        resources.getString(R.string.access_token),
+                        resources.getString(R.string.access_token)
+                    )
+                }
+            }
+        }
+        install(ContentNegotiation) {
+            json(Json { ignoreUnknownKeys = true })
+        }
+        install(HttpCache)
+    }
+
+    val repository: MastodonRepository = MastodonRepositoryImpl(client)
+
+    private val _uiState = MutableStateFlow(listOf<Status>())
+    val uiState: StateFlow<List<Status>> = _uiState.asStateFlow()
+
+    init {
+        fetchHomeTimeline()
+    }
+
+    private fun fetchHomeTimeline() {
+        viewModelScope.launch {
+            val statuses = repository.getHomeTimeline(resources.getString(R.string.id))
+
+            statuses.onSuccess { statuses ->
+                _uiState.value = statuses
+            }.onFailure { error ->
+                Napier.e("Failed to fetch home timeline", error)
+            }
+        }
+    }
+}
