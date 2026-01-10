980b9e7d610a1f60f6fde24e9ba460f8011ad422
lag129 <65485662+lag129@users.noreply.github.com>
2026-01-10 18:20:00 +0900

    TimelineScreenを分割

--- app/src/main/java/net/lag129/ferret/MainActivity.kt
+++ app/src/main/java/net/lag129/ferret/MainActivity.kt
@@ -5,26 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -35,8 +23,7 @@ import io.github.aakira.napier.Napier
 import io.ktor.http.decodeURLQueryComponent
 import io.ktor.http.encodeURLParameter
 import net.lag129.ferret.compose.MediaScreen
-import net.lag129.ferret.compose.StatusCard
-import net.lag129.ferret.compose.StatusCardData
+import net.lag129.ferret.compose.TimelineScreen
 import net.lag129.ferret.ui.theme.FerretTheme
 
 class MainActivity : ComponentActivity() {
@@ -109,73 +96,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun TimelineScreen(
-    viewModel: TimelineViewModel,
-    onNavigate: (mediaUrl: String, description: String?) -> Unit,
-    modifier: Modifier = Modifier
-) {
-    val statuses by viewModel.uiState.collectAsState()
-
-    LazyColumn(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(12.dp)
-    ) {
-        items(
-            items = statuses,
-            key = { status -> status.id }
-        ) { status ->
-            Column(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                StatusCard(
-                    data = StatusCardData(
-                        displayName = status.account.displayName,
-                        userName = status.account.acct,
-                        createdAt = status.createdAt,
-                        avatarUrl = status.account.avatar,
-                        content = status.content,
-                        card = status.card,
-                        displayNameEmojis = status.account.emojis,
-                        emojis = status.emojis,
-                        mediaAttachments = status.mediaAttachments
-                    ),
-                    onMediaClick = onNavigate
-                )
-
-                HorizontalDivider(
-                    color = MaterialTheme.colorScheme.onSurfaceVariant,
-                    thickness = 0.2.dp
-                )
-            }
-        }
-
-        val isLast = statuses.isEmpty()
-
-        if (isLast.not()) {
-            item {
-                LoadingIndicator(viewModel, statuses.last().id)
-            }
-        }
-    }
-}
-
-@Composable
-fun LoadingIndicator(
-    viewModel: TimelineViewModel,
-    maxId: String,
-    modifier: Modifier = Modifier
-) {
-    LinearProgressIndicator(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = modifier.fillMaxWidth()
-    )
-
-    LaunchedEffect(Unit) {
-        viewModel.fetchNextHomeTimeline(maxId)
     }
 }
---
+++ app/src/main/java/net/lag129/ferret/compose/TimelineScreen.kt
@@ -0,0 +1,86 @@
+package net.lag129.ferret.compose
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import net.lag129.ferret.TimelineViewModel
+
+@Composable
+fun TimelineScreen(
+    viewModel: TimelineViewModel,
+    onNavigate: (mediaUrl: String, description: String?) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val statuses by viewModel.uiState.collectAsState()
+
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(12.dp)
+    ) {
+        items(
+            items = statuses,
+            key = { status -> status.id }
+        ) { status ->
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                StatusCard(
+                    data = StatusCardData(
+                        displayName = status.account.displayName,
+                        userName = status.account.acct,
+                        createdAt = status.createdAt,
+                        avatarUrl = status.account.avatar,
+                        content = status.content,
+                        card = status.card,
+                        displayNameEmojis = status.account.emojis,
+                        emojis = status.emojis,
+                        mediaAttachments = status.mediaAttachments
+                    ),
+                    onMediaClick = onNavigate
+                )
+
+                HorizontalDivider(
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    thickness = 0.2.dp
+                )
+            }
+        }
+
+        val isLast = statuses.isEmpty()
+
+        if (isLast.not()) {
+            item {
+                LoadingIndicator(viewModel, statuses.last().id)
+            }
+        }
+    }
+}
+
+@Composable
+fun LoadingIndicator(
+    viewModel: TimelineViewModel,
+    maxId: String,
+    modifier: Modifier = Modifier
+) {
+    LinearProgressIndicator(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.fillMaxWidth()
+    )
+
+    LaunchedEffect(Unit) {
+        viewModel.fetchNextHomeTimeline(maxId)
+    }
+}
