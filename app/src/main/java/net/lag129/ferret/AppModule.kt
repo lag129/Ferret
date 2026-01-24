b84d166baecc8cd2a8cd75d802549a083088a206
lag129 <65485662+lag129@users.noreply.github.com>
2026-01-24 16:34:14 +0900

    AppModuleを更新

--- app/src/main/java/net/lag129/ferret/AppModule.kt
+++ app/src/main/java/net/lag129/ferret/AppModule.kt
@@ -1,12 +1,47 @@
 package net.lag129.ferret
 
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
+import io.ktor.client.plugins.cache.HttpCache
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
 import net.lag129.ferret.utils.DateUtils
 import net.lag129.ferret.utils.DateUtilsImpl
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
+
+    single<HttpClient> {
+        val preferencesRepository = get<PreferencesRepository>()
+        val serverName = runBlocking { preferencesRepository.serverName.first() }
+        HttpClient(CIO) {
+            defaultRequest { url("https://$serverName/") }
+            install(Auth) {
+                bearer {
+                    loadTokens {
+                        val bearerToken = preferencesRepository.bearerToken.first()
+                        BearerTokens(bearerToken, bearerToken)
+                    }
+                }
+            }
+            install(ContentNegotiation) {
+                json(Json { ignoreUnknownKeys = true })
+            }
+            install(HttpCache)
+        }
+    }
+
     single<PreferencesRepository> { PreferencesRepositoryImpl(get()) }
+
+    single<MastodonRepository> { MastodonRepositoryImpl(get()) }
 
     viewModel { TimelineViewModel(get()) }
 
--- app/src/main/java/net/lag129/ferret/TimelineViewModel.kt
+++ app/src/main/java/net/lag129/ferret/TimelineViewModel.kt
@@ -3,67 +3,26 @@ package net.lag129.ferret
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.github.aakira.napier.Napier
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.auth.Auth
-import io.ktor.client.plugins.auth.providers.BearerTokens
-import io.ktor.client.plugins.auth.providers.bearer
-import io.ktor.client.plugins.cache.HttpCache
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.defaultRequest
-import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import kotlinx.serialization.json.Json
 import net.lag129.ferret.api.entity.Status
 
 class TimelineViewModel(
-    private val preferencesRepository: PreferencesRepository
+    private val mastodonRepository: MastodonRepository,
 ) : ViewModel() {
-
-    private var client: HttpClient? = null
-    private var mastodonRepository: MastodonRepository? = null
 
     private val _uiState = MutableStateFlow(listOf<Status>())
     val uiState: StateFlow<List<Status>> = _uiState.asStateFlow()
 
     init {
-        viewModelScope.launch {
-            val serverName = preferencesRepository.serverName.first()
-            val bearerToken = preferencesRepository.bearerToken.first()
-
-            client = createHttpClient(serverName, bearerToken)
-            mastodonRepository = MastodonRepositoryImpl(client ?: return@launch)
-            fetchHomeTimeline()
-        }
-    }
-
-    private fun createHttpClient(
-        serverName: String,
-        bearerToken: String
-    ): HttpClient {
-        return HttpClient(CIO) {
-            defaultRequest { url("https://$serverName/") }
-            install(Auth) {
-                bearer {
-                    loadTokens {
-                        BearerTokens(bearerToken, bearerToken)
-                    }
-                }
-            }
-            install(ContentNegotiation) {
-                json(Json { ignoreUnknownKeys = true })
-            }
-            install(HttpCache)
-        }
+        fetchHomeTimeline()
     }
 
     private fun fetchHomeTimeline() {
         viewModelScope.launch {
-            val statuses = mastodonRepository?.getHomeTimeline() ?: return@launch
+            val statuses = mastodonRepository.getHomeTimeline()
 
             statuses.onSuccess { statuses ->
                 _uiState.value = statuses
@@ -77,9 +36,7 @@ class TimelineViewModel(
         maxId: String
     ) {
         viewModelScope.launch {
-            val statuses = mastodonRepository?.getHomeTimeline(
-                maxId = maxId
-            ) ?: return@launch
+            val statuses = mastodonRepository.getHomeTimeline(maxId)
 
             statuses.onSuccess { statuses ->
                 _uiState.value += statuses
