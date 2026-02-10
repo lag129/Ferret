7eaed6af61b38a5f2f4e4074994731eb00e6a154
lag129 <65485662+lag129@users.noreply.github.com>
2026-02-11 00:01:25 +0900

    設定画面を追加

--- app/src/main/java/net/lag129/ferret/compose/FerretTopAppBar.kt
+++ app/src/main/java/net/lag129/ferret/compose/FerretTopAppBar.kt
@@ -1,9 +1,14 @@
 package net.lag129.ferret.compose
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -13,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import net.lag129.ferret.R
 import net.lag129.ferret.Timeline
 
@@ -21,51 +27,62 @@ import net.lag129.ferret.Timeline
 fun FerretTopAppBar(
     currentTimeline: Timeline,
     onSwitch: (Timeline) -> Unit,
+    onSettingClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     var isExpanded by remember { mutableStateOf(false) }
 
     TopAppBar(
         title = {
-            Text(
-                when (currentTimeline) {
-                    Timeline.HOME -> stringResource(R.string.home_timeline)
-                    Timeline.LOCAL -> stringResource(R.string.local_timeline)
-                    Timeline.FEDERATED -> stringResource(R.string.federated_timeline)
-                },
-                modifier = Modifier.clickable { isExpanded = true }
-            )
+            Box {
+                Text(
+                    when (currentTimeline) {
+                        Timeline.HOME -> stringResource(R.string.home_timeline)
+                        Timeline.LOCAL -> stringResource(R.string.local_timeline)
+                        Timeline.FEDERATED -> stringResource(R.string.federated_timeline)
+                    },
+                    modifier = Modifier.clickable { isExpanded = true }
+                )
+
+                DropdownMenu(
+                    expanded = isExpanded,
+                    onDismissRequest = { isExpanded = false }
+                ) {
+                    DropdownMenuItem(
+                        text = { Text(stringResource(R.string.home_timeline)) },
+                        onClick = {
+                            onSwitch(Timeline.HOME)
+                            isExpanded = false
+                        }
+                    )
+
+                    DropdownMenuItem(
+                        text = { Text(stringResource(R.string.local_timeline)) },
+                        onClick = {
+                            onSwitch(Timeline.LOCAL)
+                            isExpanded = false
+                        }
+                    )
+
+                    DropdownMenuItem(
+                        text = { Text(stringResource(R.string.federated_timeline)) },
+                        onClick = {
+                            onSwitch(Timeline.FEDERATED)
+                            isExpanded = false
+                        }
+                    )
+                }
+            }
         },
         actions = {
-            DropdownMenu(
-                expanded = isExpanded,
-                onDismissRequest = { isExpanded = false }
-            ) {
-                DropdownMenuItem(
-                    text = { Text(stringResource(R.string.home_timeline)) },
-                    onClick = {
-                        onSwitch(Timeline.HOME)
-                        isExpanded = false
-                    }
-                )
-
-                DropdownMenuItem(
-                    text = { Text(stringResource(R.string.local_timeline)) },
-                    onClick = {
-                        onSwitch(Timeline.LOCAL)
-                        isExpanded = false
-                    }
-                )
-
-                DropdownMenuItem(
-                    text = { Text(stringResource(R.string.federated_timeline)) },
-                    onClick = {
-                        onSwitch(Timeline.FEDERATED)
-                        isExpanded = false
-                    }
+            IconButton(onClick = onSettingClick) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = "その他"
                 )
             }
         },
+        expandedHeight = 48.dp,
         modifier = modifier
     )
 }
