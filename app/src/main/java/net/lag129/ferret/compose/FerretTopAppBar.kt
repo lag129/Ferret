39507be23dfce30ef7cfdab6aeba5eab3e2df272
lag129 <65485662+lag129@users.noreply.github.com>
2026-02-12 17:47:08 +0900

    TextButtonに変更

--- app/src/main/java/net/lag129/ferret/compose/FerretTopAppBar.kt
+++ app/src/main/java/net/lag129/ferret/compose/FerretTopAppBar.kt
@@ -2,14 +2,11 @@ package net.lag129.ferret.compose
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -75,11 +72,8 @@ fun FerretTopAppBar(
             }
         },
         actions = {
-            IconButton(onClick = onSettingClick) {
-                Icon(
-                    imageVector = Icons.Default.MoreVert,
-                    contentDescription = "その他"
-                )
+            TextButton(onClick = onSettingClick) {
+                Text("︙")
             }
         },
         expandedHeight = 48.dp,
