60d67f045110601ccdbebd5d4c206e2e311eb8f2
lag129 <65485662+lag129@users.noreply.github.com>
2026-05-13 23:46:43 +0900

    フィードバックを追加

--- app/src/main/java/net/lag129/ferret/ui/compose/FerretBottomAppBar.kt
+++ app/src/main/java/net/lag129/ferret/ui/compose/FerretBottomAppBar.kt
@@ -8,6 +8,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import net.lag129.ferret.R
@@ -22,6 +24,8 @@ fun FerretBottomAppBar(
     onClick: (BottomAppBarItem) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val haptic = LocalHapticFeedback.current
+
     BottomAppBar(
         modifier = modifier
             .height(80.dp)
@@ -38,6 +42,7 @@ fun FerretBottomAppBar(
             label = { },
             selected = selected == BottomAppBarItem.HOME,
             onClick = {
+                haptic.performHapticFeedback(HapticFeedbackType.LongPress)
                 onClick(BottomAppBarItem.HOME)
             }
         )
@@ -53,6 +58,7 @@ fun FerretBottomAppBar(
             label = { },
             selected = selected == BottomAppBarItem.PROFILE,
             onClick = {
+                haptic.performHapticFeedback(HapticFeedbackType.LongPress)
                 onClick(BottomAppBarItem.PROFILE)
             }
         )
